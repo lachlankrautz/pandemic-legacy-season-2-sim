6b50087cfdf29477d555eea3bfe69171a21a6027
Lachlan Krautz <lachlan.krautz@gmail.com>
2025-01-31 07:47:37 +1000

    feat: config save dir

--- src/bootstrap/bootstrap.ts
+++ src/bootstrap/bootstrap.ts
@@ -1,7 +1,8 @@
 import type { CliRunner } from "../app/cli/yargs-cli-runner.ts";
 import { makeYargsCliRunner } from "../app/cli/yargs-cli-runner.ts";
 import { hideBin } from "yargs/helpers";
+import { fileRepository } from "../app/repository/file-repository.ts";
 
 export const boostrapCli = (): CliRunner => {
-  return makeYargsCliRunner(console, hideBin(process.argv));
+  return makeYargsCliRunner(console, fileRepository, hideBin(process.argv));
 };
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,19 @@
 import { randomUUID } from "crypto";
+import path from "path";
 
 // Marshal environment variables to strongly typed config
 // Do not directly access environment variables outside this config module
 // https://12factor.net/config
 export type Config = {
   seed: string;
+  saveDir: string;
 };
 
-export const config: Config = {
+const config: Config = {
   seed: process.env["SEED"] || randomUUID(),
+  saveDir: path.resolve(import.meta.dirname, "../../saves"),
+};
+
+export const getConfig = (): Config => {
+  return config;
 };
